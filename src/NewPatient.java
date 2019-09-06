8a1707c096d094d783ceb2daf6cdb07d29043043
FaierAndreea <andreea.faier@gmail.com>
2019-09-06 21:50:51 +0300

    new commit

--- src/NewPatient.java
+++ src/NewPatient.java
@@ -152,6 +152,33 @@ public class NewPatient extends javax.swing.JFrame {
     }// </editor-fold>
 
 
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewPatient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewPatient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewPatient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewPatient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewPatient().setVisible(true);
+            }
+        });
+    }
 
     private void signupButtonMouseClicked(java.awt.event.MouseEvent evt) {
         String fulln=fullnameTextField.getText();
